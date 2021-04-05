7472be01f5d236149fc81d395f5c27cac937c764
aziz ibragimov <azizconi1@gmail.com>
2021-04-05 21:23:52 +0500

    new NotesVersion

--- app/src/main/java/com/example/notes/DataBase/Note.java
+++ app/src/main/java/com/example/notes/DataBase/Note.java
@@ -30,14 +30,49 @@ public class Note {
     public String date;
 
 
-//    public Note() {
-//    }
+    public Note() {
+    }
 
-//    public Note(String title, String text) {
-//        this.title = title;
-//        this.text = text;
-//    }
+    //Дата не добавлен потому что для него пишется мотод в самом MainActivity
+    public Note(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
 
+    //Getter
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    //Setter
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 
 
 
