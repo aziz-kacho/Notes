fcb8f8631074c8ebf212f16ad8300209f7427f40
aziz ibragimov <azizconi1@gmail.com>
2021-04-05 22:01:12 +0500

    2vers

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -2,18 +2,7 @@ package com.example.notes;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
-
-import com.example.notes.DataBase.App;
-import com.example.notes.DataBase.DataBase;
-import com.example.notes.DataBase.Note;
-import com.example.notes.DataBase.NoteDao;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
